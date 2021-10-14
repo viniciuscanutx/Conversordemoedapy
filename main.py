168123b1142ba5bf3f6200e6aa75be8606111b99
viniciuscanutx <viniciuscanutz@gmail.com>
2021-10-14 20:25:18 -0300

    Comentando o Projeto e finalizando

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+#Bibliotecas usadas no projeto.
 import requests
 import sys
 
+#Aqui a biblioteca requests vai fazer a requisição para conseguir a cotação do dólar em tempo real!
 requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL") 
 requisicao_dic = requisicao.json()
 cotacao_dolar = requisicao_dic["USDBRL"]['bid']
 
-
+#Aqui criamos aonde vai ser feita a conversão e vai ser exibida na tela do usuário.
 def convertpapai():
 
  while True:
@@ -14,6 +16,7 @@ def convertpapai():
      valor = input("Deseja converter pra Reais (BRL) ou Dolares (USD)? ")
      print("\n")
 
+#Aqui tive que usar float porque os válores são quebrados.
      if valor == "1":
       flowers = float(cotacao_dolar)
       print("Cotação atual do Dólar =",cotacao_dolar)
@@ -34,13 +37,15 @@ def convertpapai():
       print("O resultado da conversão de USD para BRL é:\n RS$:",valorresult2)
       print("\n")
       choiceuser2()
-    
+
+#Caso o usuário digite a opção errada ele retornará para o menu, assim, impedindo que o programa crashe.
      else:
       print("Opção Inválida!")
       print("\n")
       return convertpapai()
 
 
+#Aqui é uma pequena tela de seleção para saber se o usuário deseja fazer alguma outra conversão.
 def choiceuser2():
     
  while True:
@@ -49,9 +54,12 @@ def choiceuser2():
   notime = input("Deseja converter outro valor?\n==> ")
  
   if notime == "1":
+    print("\n")
     return convertpapai()
 
   elif notime == "2":
+   print("\n")
+   print("Good Bye =)")
    return fechar()
 
   else:
@@ -60,11 +68,11 @@ def choiceuser2():
     return choiceuser2()
 
 
-
+#Prefiro usar a biblioteca sys para fazer o comando de fechar o programa pois evita bugs e vou compilar em .exe depois.
 def fechar():
     sys.exit()
 
 
-
+#Sem isso, o programa não roda!
 convertpapai()
 choiceuser2()
